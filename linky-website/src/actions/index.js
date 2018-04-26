5867e3e22d4eb753b63b365a670ca152341bfe52
Harshad <harshad@Harshads-MacBook-Pro.local>
2018-04-26 23:37:40 +0530

    [web/hotfix] Changed API endpoint

--- linky-website/src/actions/index.js
+++ linky-website/src/actions/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:3060';
+const API_BASE_URL = 'https://api.harshad.nl/linky';
 
 export const LOADING_TAGS = 'LOADING_TAGS';
 export const LOADED_TAGS = 'LOADED_TAGS';
