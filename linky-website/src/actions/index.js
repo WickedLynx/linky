dab469043890186aebe4f8893b6e1ae9a497207b
Harshad <harshad@Harshads-MacBook-Pro.local>
2018-04-28 21:08:48 +0530

    [website] Switched to prod endpoint

--- linky-website/src/actions/index.js
+++ linky-website/src/actions/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import AuthStore from '../other/auth_store';
 import Cache from '../other/cache';
 
-const API_BASE_URL = 'http://localhost:3060';
+const API_BASE_URL = 'https://api.harshad.nl/linky';
 
 export const LOADING_TAGS = 'LOADING_TAGS';
 export const LOADED_TAGS = 'LOADED_TAGS';
